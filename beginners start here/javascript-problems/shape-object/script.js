3f3cfdea39bf0f1aaca3c24c4582b69351814325
hyfyooie <hyfyooie@outlook.com>
2020-03-23 19:29:35 +0700

    Add ES6 'class' version of the Constructor Function

--- beginners start here/javascript-problems/shape-object/script.js
+++ beginners start here/javascript-problems/shape-object/script.js
@@ -1,13 +1,28 @@
-function Shape (name, sides, sideLength) {
-  this.name = name;
-  this.sides = sides;
-  this.sideLength = sideLength;
+// function Shape(name, sides, sideLength) {
+//   this.name = name;
+//   this.sides = sides;
+//   this.sideLength = sideLength;
+// }
+//
+// Shape.prototype.calcPerimeter = function() {
+//   return `The perimeter of this ${this.name} is ${this.sides * this.sideLength}`;
+// };
+
+// Via ES6 'Class'
+
+class Shape {
+  constructor(name, sides, sideLength) {
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+  }
+
+  calcPerimeter() {
+    return `The perimeter of this ${this.name} is ${this.sides * this.sideLength}`;
+  }
 }
 
-Shape.prototype.calcPerimeter = function() {
-  return `The perimeter of this ${this.name} is ${this.sides * this.sideLength}`;
-};
+// Instances
 
 let square = new Shape('Square', '4', '5');
-
 let triangle = new Shape('Triangle', '3', '3');
