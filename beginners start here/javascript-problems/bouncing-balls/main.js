3318e4188fc24774a79d4bdab8eddf7a8ae686d3
hyfyooie <hyfyooie@outlook.com>
2020-03-25 18:33:31 +0700

    Add 'random' function

--- beginners start here/javascript-problems/bouncing-balls/main.js
+++ beginners start here/javascript-problems/bouncing-balls/main.js
@@ -4,3 +4,9 @@ const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+
+// This function takes towo numbers as arguments, and returns a random number in the range between the two
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
