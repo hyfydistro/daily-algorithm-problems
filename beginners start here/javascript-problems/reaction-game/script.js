6af4eb668a9c6045869b7bfe1fc761fa5293a185
hyfyooie <hyfyooie@outlook.com>
2020-04-05 18:07:40 +0700

    Add function presets

--- beginners start here/javascript-problems/reaction-game/script.js
+++ beginners start here/javascript-problems/reaction-game/script.js
@@ -5,3 +5,24 @@ let startTime = null;
 let rAF;
 const btn = document.querySelector('button');
 const result = document.querySelector('.result');
+
+function random(min, max) {
+  let num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+}
+
+function draw(timestamp) {
+  if (!startTime) {
+    startTime = timestamp;
+  }
+
+  rotateCount = (timestamp - startTime) / 3;
+
+  if (rotate > 359) {
+    rotateCount %= 360;
+  }
+
+  spinner.style.transform = `rotate(${rotateCount}deg)`;
+  
+  rAF = requestAnimationFrame(draw);
+}
