b09835ae0ea7a47970b687f980f8a8a6d828535d
hyfyooie <hyfyooie@outlook.com>
2020-04-05 18:01:17 +0700

    Initialise setup - WIP

---
+++ beginners start here/javascript-problems/reaction-game/script.js
@@ -0,0 +1,7 @@
+const spinner = document.querySelector('.spinner p');
+const spinnerContainer = document.querySelector('.spinner');
+let rotateCount = 0;
+let startTime = null;
+let rAF;
+const btn = document.querySelector('button');
+const result = document.querySelector('.result');
