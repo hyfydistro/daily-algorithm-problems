f769928a88208abb2b79e8e5f84dfb3f52f49e04
hyfyooie <hyfyooie@outlook.com>
2020-04-17 14:47:09 +0700

    Create elements; Set elements and append them to list; Add event listener to remove itself

--- beginners start here/javascript-problems/add-remove-items-from-shopping-list/script.js
+++ beginners start here/javascript-problems/add-remove-items-from-shopping-list/script.js
@@ -4,18 +4,36 @@ const input = document.querySelector('input');
 
 function response() {
   // Store input value
-  let value = input.value.toString();
+  let value = input.value;
   console.log(value);
 
 // Throw error if...
-  if (value === '') {
-    // Write a warning and prevent from adding blank slots
-  }
+  // if (value === '') {
+  //   // Write a warning and prevent from adding blank slots
+  // }
 
   // Empty string
   input.value = '';
 
   // Create 3 new elements: li, span, button and store in variables
+  let newListItem = document.createElement('li');
+  let newSpan = document.createElement('span');
+  let newBtn = document.createElement('button');
+
+  newListItem.appendChild(newSpan);
+  newListItem.appendChild(newBtn);
+
+  newSpan.textContent = value;
+  newBtn.textContent = 'Delete';
+
+  list.appendChild(newListItem);
+
+  newBtn.addEventListener('click', function() {
+    newListItem.remove();
+    // list.removeChild(newListItem);
+  });
+
+
 }
 
 btn.addEventListener('click', response);
