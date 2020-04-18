74991585ee702f13e19d6a1037f233bfe45b3cfa
hyfyooie <hyfyooie@outlook.com>
2020-04-18 16:34:52 +0700

    Add XHR process

--- beginners start here/javascript-problems/simple-ajax-request/script.js
+++ beginners start here/javascript-problems/simple-ajax-request/script.js
@@ -7,7 +7,19 @@ verseChoose.onchange = function() {
 };
 
 function updateDisplay(verse) {
+  // Make text equivalent to text file path name
   verse = verse.replace(" ", "");
   verse = verse.toLowerCase();
   let url = verse + '.txt';
+
+  // XHR request
+  let request = new XMLHttpRequest();
+  // specify request method
+  request.open('GET', url);
+  // Set response to expext; xhr return 'text' as default
+  request.responseType = 'text';
+  request.onload = function() {
+    poemDisplay.textContent = request.response;
+  };
+  request.send();
 }
