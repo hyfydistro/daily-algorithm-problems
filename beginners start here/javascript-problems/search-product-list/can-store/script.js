9dd824334670673862f3272de6fb724b7f9e17fb
hyfyooie <hyfyooie@outlook.com>
2020-04-21 16:34:17 +0700

    Create functions (WIP)

--- beginners start here/javascript-problems/search-product-list/can-store/script.js
+++ beginners start here/javascript-problems/search-product-list/can-store/script.js
@@ -1,9 +1,19 @@
+// create a variable to store the products 'database' in
+let products;
+
+// use fetch to retrieve it, and report any errors that occur in the fetch operation
+// once the products have been successfully loaded and formatted as a JSON object
+// using response.json(), run the initialize() function
 // Get JSON
 // Promise
 fetch('products.json').then(function(response) {
-  return response.json();
+  if (response.ok) {
+    return response.json();
+  } else {
+    console.error("Unable to retrieve file");
+  }
 }).then(function(json) {
-  let products = json;
+  products = json;
   initialize(products);
 }).catch(function(err) {
   console.log('Fetch problem: ' + err.message);
@@ -32,6 +42,31 @@ xhr.onerror = function() {
 
 xhr.send();
 
+// Sets up the app locgic, declares required variables, contains all the other functions.
+function initialize() {
+  // grab the UI elements that we need to manipulate
+
+  // ... Add the rest of the functions inside!
+}
+
+// selectProducts() Takes the group of products selected by selectCategory(), and further
+// filters them by the entered search term (if one has been entered)
+function selectProducts() {
+  // add code here
+}
+
+// Start the process of updating the display with the new set of products
+function updateDisplay() {
+  // add code here
+}
+
+// FetchBlob uses fetch to retrieve the image from that product, and then sends the
+// resulting image display URL and product on to showProduct() to finally
+// display it
+
+function fetchBlob(product) {
+  // add code here
+}
 
 // Get Image from JSON
 //Promise
@@ -64,3 +99,8 @@ xhr.onerror = function() {
 };
 
 xhr.send();
+
+// Display a product inside the <main> element
+function showProduct(objectURL, product) {
+
+}
