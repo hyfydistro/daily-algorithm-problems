f1fb5b23a4fd3f7ab71ddf63e707dc4a6f57ba89
hyfyooie <hyfyooie@outlook.com>
2020-04-22 20:24:28 +0700

    Add Modern Browser code

--- beginners start here/javascript-problems/api-mapquest/script.js
+++ beginners start here/javascript-problems/api-mapquest/script.js
@@ -10,4 +10,16 @@ window.onload = function() {
     layers: L.mapquest.tileLayer('map'),
     zoom: 12
   }) ;
-}
+};
+
+// Modern Browser support
+window.addEventListener('load',  function() {
+  L.mapquest.key = 'YOUR-API-KEY-HERE';
+
+  // 'map' refers to a <div> element with the ID map
+  let map = L.mapquest.mapquest('map', {
+    center: [53.480759, -2.242631],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+  });
+});
